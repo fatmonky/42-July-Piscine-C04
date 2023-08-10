0b22c451d5e64b73868669c1e5fa10cb5c19dc98
Peijing Teh <pteh@c1r1s2.42singapore.sg>
2023-08-10 17:38:10 +0800

    corrected silly error: lack of () in ex00.

--- ex00/ft_strlen.c
+++ ex00/ft_strlen.c
@@ -6,19 +6,27 @@
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 17:28:05 by pteh              #+#    #+#             */
-/*   Updated: 2023/08/07 17:30:55 by pteh             ###   ########.fr       */
+/*   Updated: 2023/08/10 17:37:27 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_strlen(char *str)
 {
 	int	count;
 
 	count = 0;
-	while *str
+	while (*str)
 	{
-	count++;
-	str++;
+		count++;
+		str++;
+	}
+	return (count);
 }
-return (count);
-}
+/*
+int	main(void)
+{
+	char	i[10] = "Hello";
+	printf("length of hello: %d\n", ft_strlen(i));
+}*/
