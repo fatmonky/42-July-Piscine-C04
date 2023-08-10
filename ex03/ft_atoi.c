5b8fcb6cec57e7920fb4437b5f06641059bd8026
Peijing Teh <pteh@c1r1s2.42singapore.sg>
2023-08-10 16:35:54 +0800

    working version, but too many lines of code... sad.

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 11:19:08 by pteh              #+#    #+#             */
-/*   Updated: 2023/08/10 16:31:31 by pteh             ###   ########.fr       */
+/*   Updated: 2023/08/10 16:35:30 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,6 @@
 #include <stdio.h>
 #include <limits.h>
 
-// Requirements:
-// - if string no longer reads ints, then stops.
-// - return integer value, or 0 if no integers in whole string. 
-// - if string has odd number of -, then it will return '-'.
-// - if string starts with any numb of whitespace chars (space, \f, \n, \r, \t, \v)
-/*
-int	check_int(char c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	else
-		return (0);
-}
-
-int	check_whitespace(char c)
-{
-	if ((c >= 9 && c <= 13) || c == 32)
-		return (1);
-	else
-		return (0);
-}
-
-int	check_negative(char c)
-{
-//helper function to check if negative: return 1 if yes. 
-	if (c == '-')
-		return (1);
-	else
-		return (0);
-}
-*/
 int	ft_atoi(char *str)
 {
 	int	negative_count;
@@ -55,8 +24,8 @@ int	ft_atoi(char *str)
 	return_val = 0;
 	negative_count = 0;
 	c = 0;
-	while ((str[c] > 8 && str[c] <= 13) 
-			|| (str[c] == 32)) 
+	while ((str[c] > 8 && str[c] <= 13)
+		|| (str[c] == 32))
 		c++;
 	while (str[c] == '+' || str[c] == '-')
 	{
@@ -75,18 +44,18 @@ int	ft_atoi(char *str)
 	else
 		return (return_val);
 }
+/*
+   int	main(void)
+   {
+   char	wrongstart[30] = "  ---+--+1234ab567";
+   char	wrongmid[15] = "1234ab567";
+   char	wrongend[15] = "1234567abcd";
+   char	wrongall[15] = "abcdefg";
+   char	wrongminuses[30] = "  ---+-+1234ab567";
 
-int	main(void)
-{
-	char	wrongstart[30] = "  ---+--+1234ab567";
-	char	wrongmid[15] = "1234ab567";
-	char	wrongend[15] = "1234567abcd";
-	char	wrongall[15] = "abcdefg";
-	char	wrongminuses[30] = "  ---+-+1234ab567";
-
-	printf("ft-wrongstart:%d\n",ft_atoi(wrongstart));
-	printf("ft-wrongmid:%d\n", ft_atoi(wrongmid));
-	printf("ft-wrongend: %d\n",ft_atoi(wrongend));
-	printf("ft-wrongall: %d\n",ft_atoi(wrongall));
-	printf("ft-wrongminuses:%d\n",ft_atoi(wrongminuses));
-}
+   printf("ft-wrongstart:%d\n",ft_atoi(wrongstart));
+   printf("ft-wrongmid:%d\n", ft_atoi(wrongmid));
+   printf("ft-wrongend: %d\n",ft_atoi(wrongend));
+   printf("ft-wrongall: %d\n",ft_atoi(wrongall));
+   printf("ft-wrongminuses:%d\n",ft_atoi(wrongminuses));
+   }*/
